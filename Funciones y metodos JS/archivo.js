3106ee505cbdc31d65fa798d4e0e7a7e02189fe4
PauloAcc <paulo.matias.accardo@gmail.com>
2023-10-02 21:23:18 -0300

    push carpetas hasta 02/10/23

---
+++ Funciones y metodos JS/archivo.js
@@ -0,0 +1,63 @@
+// Seleccionar un elemento del DOM por su ID
+document.getElementById('miElemento');
+
+// Seleccionar elementos del DOM por su clase
+document.getElementsByClassName('miClase');
+
+// Seleccionar elementos del DOM por su etiqueta
+document.getElementsByTagName('div');
+
+// Agregar un evento a un elemento HTML
+elemento.addEventListener('click', function() {
+  // Código a ejecutar cuando se hace clic en el elemento
+});
+
+// Cambiar el contenido de un elemento HTML
+elemento.innerHTML = 'Nuevo contenido';
+
+// Obtener el valor de un campo de entrada
+var valor = inputElement.value;
+
+// Cambiar el valor de un campo de entrada
+inputElement.value = 'Nuevo valor';
+
+// Crear un nuevo elemento HTML
+var nuevoElemento = document.createElement('div');
+
+// Agregar un elemento al DOM
+parentElement.appendChild(nuevoElemento);
+
+// Eliminar un elemento del DOM
+parentElement.removeChild(elemento);
+
+// Definir una función en JavaScript
+function miFuncion(parametro1, parametro2) {
+  // Código de la función
+}
+
+// Llamar a una función
+miFuncion(valor1, valor2);
+
+// Declarar una variable
+var nombreVariable = 'valor';
+
+// Condición if
+if (condicion) {
+  // Código si la condición es verdadera
+} else {
+  // Código si la condición es falsa
+}
+
+// Bucle for
+for (var i = 0; i < 5; i++) {
+  // Código a repetir en cada iteración
+}
+
+// Objeto JavaScript
+var miObjeto = {
+  propiedad1: 'valor1',
+  propiedad2: 'valor2',
+  metodo: function() {
+    // Código del método
+  }
+};
